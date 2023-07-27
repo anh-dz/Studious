8af6840e9bdc783fc478e67afa3acc80c222fce3
Huynguyen736 <nguyenhuy07032006@gmail.com>
2023-07-27 21:40:22 +0700

    add start/stop timer and music function

--- control/mainControl.py
+++ control/mainControl.py
@@ -1,33 +1,79 @@
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+from PyQt6.QtMultimedia import *
 from main import *
-import time
+from random import choice
+import sys
 
 class StudiousFunc:
     def __init__(self, widgets):
         super().__init__()
         global wgs
         wgs = widgets
-        wgs.btn_lB_pom.clicked.connect(self.testBackend)
 
-    
-    def testBackend(self):
-        wgs.label.setText("UR GAY")
+        #random qoutes and print in app
+        list_quotes = ["Chúng ta có thể gặp nhiều thất bại nhưng chúng ta không được bị đánh bại – Maya Angelou",
+                       "Tất cả những sự khó khăn thường là để chuẩn bị cho những người bình thường một số phận phi thường – C.S. Lewis",
+                       "Mọi người sẽ quên những gì bạn nói, quên những gì bạn đã làm, nhưng họ sẽ không bao giờ quên cảm xúc mà bạn mang lại cho họ – Maya Angelou."]
+        wgs.lb_m_quote.setText(f"{choice(list_quotes)}")
+
+        #connect start/stop button with clock
+        wgs.btn_m_startstop.clicked.connect(self.start_clock)
         self.countdown = countdown()
-        self.countdown.start_timer()
-        print("Hello World")
+        self.clock_onoff = False
+
+        #Put clock to next session
+        wgs.btn_m_next.clicked.connect(self.next_clock)
+
+        #Turn on/off music in app
+        wgs.btn_m_audio.clicked.connect(self.onoff_audio)
+        self.media_content = QUrl.fromLocalFile('.\\assert\\music.wav')
+        self.media_player = QSoundEffect()
+        self.media_player.setSource(self.media_content)
+        self.media_player.setLoopCount(-2) # possible bug: QSoundEffect::Infinite cannot be used in setLoopCount
+        self.music_onoff = True
+        self.media_player.setVolume(1)
+        self.onoff_audio()
+    
+    def start_clock(self):
+        if self.clock_onoff == False:
+            print("Start")
+            self.clock_onoff = True
+            self.countdown.start_timer()
+        elif self.clock_onoff == True:
+            print("Stop")
+            self.clock_onoff = False
+            self.countdown.stop_timer()
+    
+    def next_clock(self):
+        self.countdown = None
+        self.countdown = countdown()
+
+    def onoff_audio(self):
+        if self.music_onoff:
+            print("start")
+            self.media_player.play()
+            self.music_onoff = False
+        elif self.music_onoff == False:
+            print("stop")
+            self.media_player.stop()
 
 class countdown:
     def __init__(self):
-        self.mtime = 1
+        self.mtime = 25
+        wgs.lb_m_time.setText(f"{self.mtime}:00")
         self.start_time = self.mtime*60
         self.time_left = self.start_time
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update_countdown)
 
     def start_timer(self):
+        self.timer.timeout.connect(self.update_countdown)
         self.timer.start(1000)  # Update every second
+
+    def stop_timer(self):
+        try: self.timer.disconnect()
+        except: pass
 
     def update_countdown(self):
         self.time_left -= 1
@@ -35,7 +81,7 @@ class countdown:
         if self.time_left >= 0:
             # Format the remaining time as MM:SS
             minutes, seconds = divmod(self.time_left, 60)
-            wgs.label.setText(f"{minutes:02}:{seconds:02}")
+            wgs.lb_m_time.setText(f"{minutes:02}:{seconds:02}")
         else:
             self.timer.stop()
-            wgs.label.setText("Time's up!")
+            wgs.lb_m_time.setText("00:00")
