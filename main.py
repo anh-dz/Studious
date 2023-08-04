435fcbeae11c125370a31d4074cce2910b8d6aad
Huynguyen736 <nguyenhuy07032006@gmail.com>
2023-08-04 13:30:56 +0700

    fix don't show app icon in taskbar windows

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from PyQt6.QtWidgets import QApplication
 from view import *
 from control import *
 import os
+import ctypes
+
+myappid = 'thedreamteam.studious.app' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 os.environ["QT_FONT_DPI"] = "72" # FIX Problem for High DPI and Scale above 100%
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
